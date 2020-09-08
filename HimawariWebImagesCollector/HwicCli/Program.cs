f931bd3ae1ed55e9fb6d57d7871a240a4b2903a3
lino <lino_snsalias@outlook.com>
2020-09-09 00:47:58 +0800

    Adds cmdl options impl

--- HimawariWebImagesCollector/HwicCli/Program.cs
+++ HimawariWebImagesCollector/HwicCli/Program.cs
@@ -1,12 +1,21 @@
-﻿using System;
-
-namespace HwicCli
+﻿namespace Hwic
 {
-    class Program
+    using System;
+
+    using System.Collections.Generic;
+
+
+    using CommandLine;
+
+
+    public class Program
     {
-        static void Main(string[] args)
+        public const string K_CONFIG_FILE = "config-file";
+
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
---
+++ HimawariWebImagesCollector/HwicCli/SyncOptions.cs
@@ -0,0 +1,44 @@
+﻿namespace Hwic
+{
+    using CommandLine;
+
+
+    [Verb("sync", HelpText = "Synchronize images from Himawari sources to remote S3")]
+    public class SyncOptions
+    {
+        [Option('s', "src", Required = true, HelpText = "Specifiy the source type of Himawari images.")]
+        public string Source { get; set; }
+
+
+        [Option('d', "dst", Required = true, HelpText = "Specify the predefined destination name where the images will be stored")]
+        public string Dest { get; set; }
+
+
+        [Option("ak", Required = false, HelpText = "Access Key to S3")]
+        public string AccessKey { get; set; }
+
+
+        [Option("sk", Required = false, HelpText = "Secret key paired with access key to S3")]
+        public string SecretKey { get; set; } 
+
+
+        [Option("src-proxy", Required = false)]
+        public string SrcProxy { get; set; }
+
+
+        [Option("dst-proxy", Required = false)]
+        public string DstProxy { get; set; }
+
+
+        [Option("all-proxy", Required = false)]
+        public string AllProxy { get; set; }
+
+
+        [Option("dry-run", Default = false)]
+        public bool DryRun { get; set; }
+
+
+        [Option('o', "output", Required = false, HelpText = "Output the Uris needed to synchronize to a file")]
+        public string Output { get; set; }
+    }
+}
