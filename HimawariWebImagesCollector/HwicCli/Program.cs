27e1fa169a2b0caf80a0025680bece90c6513373
lino <lino_snsalias@outlook.com>
2020-09-09 10:45:04 +0800

    minor update

--- HimawariWebImagesCollector/HwicCli/Program.cs
+++ HimawariWebImagesCollector/HwicCli/Program.cs
@@ -4,6 +4,9 @@
 
     using System.Collections.Generic;
 
+    using System.Threading;
+    using System.Threading.Tasks;
+
 
     using CommandLine;
 
@@ -13,9 +16,27 @@
         public const string K_CONFIG_FILE = "config-file";
 
 
-        public static void Main(string[] args)
+        public static Task Main(string[] args)
         {
+            return CommandLine.Parser.Default
+                .ParseArguments<SyncOptions>(args)
+                .MapResult(
+                    RunWithSyncOptionsAsync_,
+                    HandleParseErrorsAsync_
+                );
+        }
 
+
+        private static async Task<int> RunWithSyncOptionsAsync_(SyncOptions syncOptions)
+        {
+            await Task.CompletedTask;
+            return 0;
+        }
+
+
+        private static Task<int> HandleParseErrorsAsync_(IEnumerable<CommandLine.Error> errors)
+        {
+            return Task.FromResult(-1);
         }
     }
 }
